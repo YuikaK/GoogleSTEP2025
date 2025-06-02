f378682dceb641abe45ab081f34ff0916f9a3433
YuikaK <yuiyui2004yuiyui@icloud.com>
2025-06-02 10:58:31 +0900

    used a dictionary

--- Week1/Homework1.py
+++ Week1/Homework1.py
@@ -1,3 +1,4 @@
+"""
 def anagram_solution(random_word, dictionary):
     sorted_random_word = sorted(random_word) # ランダムな文字列をソート
 
@@ -14,7 +15,7 @@ def binary_search(sorted_dictionary, sorted_word):
     anagram = []
     left, right = 0, len(sorted_dictionary) - 1
     while left <= right:
-        mid = left + (right - left) // 2 # 質問する "mid = left + right // 2" オーバーフロー？
+        mid = left + (right - left) // 2 
         if sorted_dictionary[mid][0] < sorted_word: # 中央よりあとにあるとき
             left = mid + 1
         elif sorted_dictionary[mid][0] > sorted_word: # 中央より前にあるとき
@@ -40,15 +41,33 @@ def binary_search(sorted_dictionary, sorted_word):
                 lower -= 1  
             break
     return anagram
+"""
+
+def build_anagram_dict(dictionary): # 辞書型を返す関数
+    anagram_dict = {} # key:ソートされた文字列 value:元の単語
+    for word in dictionary:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word in anagram_dict: # 辞書にすでに入っていたら
+            anagram_dict[sorted_word].append(word) # keyに元の単語を追加する
+        else:
+            anagram_dict[sorted_word] = [word] # 入っていなかったら辞書にその言葉を入れる
+    return anagram_dict
+
+def anagram_solution(random_word, anagram_dict): # ランダムな文字列が辞書型に含まれているかを調べる関数
+    sorted_random_word = ''.join(sorted(random_word)) # ランダムな文字列をソートしてキーを作成
+    return anagram_dict.get(sorted_random_word, []) # 辞書からそのキーに対応するリストを取り出す　見つからなければ空リストを返す
 
 def main():
     random_word = input()
-    f = open("anagram/words.txt", 'r') # 読み込みモードで開く
-    dictionary = f.readlines()
-    for i in range(len(dictionary)):
-        dictionary[i] = dictionary[i].replace('\n', '') # 改行を取り除く
-    print(anagram_solution(random_word, dictionary))
-    f.close()
+    import time
+    start = time.perf_counter()
+    with open("anagram/words.txt", 'r') as f:
+        dictionary = [word.strip() for word in f.readlines()]  
+    anagram_dict = build_anagram_dict(dictionary)  # 辞書型に変換
+    result = anagram_solution(random_word, anagram_dict)
+    print(result)
+    end = time.perf_counter() #計測終了
+    print('{:.2f}'.format(end-start))
 
 if __name__ == "__main__":
     main()
